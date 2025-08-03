77a9d671b59a74655e84296d0eba625aa50708ae
@mohammedamir.khan <mohammedamir.khan@npstx.com>
2025-08-03 17:22:45 +0530

    change cors dedualt to express

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SocketIoAdapter } from './adopters/socket-io.adapter';
-import * as fs from 'fs';
-import * as path from 'path';
+import * as express from 'express';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['https://chatme-chi-gray.vercel.app'], // Allow all origins (for development only)
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true,
-  });
+  app.use(
+    cors({
+      origin: 'https://chatme-chi-gray.vercel.app',
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    }),
+  );
   app.useWebSocketAdapter(new SocketIoAdapter(app));
   const port = process.env.PORT || 5000;
   await app.listen(port, '0.0.0.0');

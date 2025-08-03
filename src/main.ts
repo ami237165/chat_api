bc8137b014e5b88cde631c05625ea888a197eedb
@mohammedamir.khan <mohammedamir.khan@npstx.com>
2025-08-03 16:38:30 +0530

    laal ssl deleted

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,15 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 async function bootstrap() {
-   const httpsOptions = {
-  key: fs.readFileSync(path.join(__dirname, "../src/ssl", "key.pem")),
-  cert: fs.readFileSync(path.join(__dirname, "../src/ssl", "cert.pem")),
-};
-  const app = await NestFactory.create(AppModule,{httpsOptions});
+  const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin:['https://chatme-chi-gray.vercel.app'],  // Allow all origins (for development only)
+    origin: ['https://chatme-chi-gray.vercel.app'], // Allow all origins (for development only)
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
   app.useWebSocketAdapter(new SocketIoAdapter(app));
   const port = process.env.PORT || 5000;
-await app.listen(port, '0.0.0.0');
+  await app.listen(port, '0.0.0.0');
   console.log(await app.getUrl());
-  
 }
 bootstrap();
